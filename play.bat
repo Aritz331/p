9f59106bb03e3c027b0e9732ed963383af760372
stivengiv <94831607+stivengiv@users.noreply.github.com>
2022-03-14 20:54:19 +0100

    Add files via upload

--- play.bat
+++ play.bat
@@ -1,6 +1,13 @@
 @echo off
+
+if [%1]==[/f] (if [%2]==[] (exit /b) else (set "fiel=%2" && set "o=fiel" && goto f))
+
+:get
 curl -k "https://stivengiv.github.io/mp3/%1.mp3" -o "%temp%\b.mp3" -s
-set "file=%temp%\b.mp3"
+set "fiel=%temp%\b.mp3"
+
+:f
+set "file=%fiel%"
 ( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
   echo Sound.URL = "%file%"
   echo Sound.Controls.play
@@ -8,12 +15,15 @@ set "file=%temp%\b.mp3"
   echo wscript.sleep 100
   echo loop
   echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) > %temp%\a.vbs
+
 start "" /min %temp%\a.vbs
+
 ping localhost -n 1 >nul
 ping localhost -n 1 >nul
 ping localhost -n 1 >nul
 ping localhost -n 1 >nul
 ping localhost -n 1 >nul
+
 del %temp%\a.vbs
-del %temp%\b.mp3
+if not "%o%"=="fiel" (del %temp%\b.mp3)
 exit /b
