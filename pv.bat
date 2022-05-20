c5520f0fba845fcd13e520ea64f7d2626f280f27
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-05-20 21:28:52 +0200

    Add files via upload

--- pv.bat
+++ pv.bat
@@ -1,20 +1,14 @@
 @echo off
 
-if [%1]==[/f] (if [%2]==[] (exit /b) else (set "fiel=%2" && set "o=fiel" && goto f))
+if [%1]==[/f] (if [%2]==[] (exit /b) else (set "fiel=%2" && set "o=fiel" && goto v))
 
 :get
-curl -kLOs stivengiv.github.io/mp4/%1.mp4 -O aritz331.github.io/ff/ffplay.exe
-set "fiel=%temp%\b.mp3"
+curl -kLs stivengiv.github.io/mp4/%1.mp4 -o %temp%\b.mp4
+curl -kLOs aritz331.github.io/ff/ffplay.exe
+set "fiel=%temp%\b.mp4"
 
 :v
 ffplay %fiel% -fs
 
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-
-del %temp%\a.vbs
 if not "%o%"=="fiel" (del %temp%\b.mp3)
 exit /b
